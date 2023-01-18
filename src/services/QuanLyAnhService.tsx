96f7f84b8ae6f1d7010f0cc3d7fa1e19761045e1
Duong Duc Anh <duongdoican@gmail.com>
2023-01-18 10:22:39 +0700

    update

--- src/services/QuanLyAnhService.tsx
+++ src/services/QuanLyAnhService.tsx
@@ -1,50 +1,50 @@
-import request from "common/Request"
+import request from "common/Request";
 const Por_Anh = "Por_Anh";
-const QuanLyAnhService = {    
-    GetItems: async () => {        
-        let res:any = await request({
-            url: `/${Por_Anh}/List?page=1&pageSize=1000&totalLimitItems=0&searchBy=`,
-            method: 'get'
-        });
-        return res
-    },
-    GetCategories: async () => {        
-        let res:any = await request({
-            url: `/${Por_Anh}/Categories`,
-            method: 'get'
-        });
-        return res
-    },
-    GetItem: async (id:String) => {        
-        let res:any = await request({
-            url: `/${Por_Anh}/${id}`,
-            method: 'get'
-        });
-        return res
-    },
-    CreateItem: async (data:any) => {        
-        let res:any = await request({
-            url: `/${Por_Anh}`,
-            method: 'post',
-            data
-        });
-        return res
-    },
-    UpdateItem: async (data:any) => {        
-        let res:any = await request({
-            url: `/${Por_Anh}`,
-            method: 'put',
-            data
-        });
-        return res
-    },
-    DeleteById: async (data:any) => {        
-        let res:any = await request({
-            url: `/${Por_Anh}`,
-            method: 'delete',
-            data        
-        });
-        return res
-    },
-}
-export default QuanLyAnhService
+const QuanLyAnhService = {
+  GetItems: async () => {
+    let res: any = await request({
+      url: `/${Por_Anh}/List?page=1&pageSize=1000&totalLimitItems=0&searchBy=`,
+      method: "get",
+    });
+    return res;
+  },
+  GetCategories: async () => {
+    let res: any = await request({
+      url: `/${Por_Anh}/Categories`,
+      method: "get",
+    });
+    return res;
+  },
+  GetItem: async (id: String) => {
+    let res: any = await request({
+      url: `/${Por_Anh}/${id}`,
+      method: "get",
+    });
+    return res;
+  },
+  CreateItem: async (data: any) => {
+    let res: any = await request({
+      url: `/${Por_Anh}`,
+      method: "post",
+      data,
+    });
+    return res;
+  },
+  UpdateItem: async (data: any) => {
+    let res: any = await request({
+      url: `/${Por_Anh}`,
+      method: "put",
+      data,
+    });
+    return res;
+  },
+  DeleteById: async (data: any) => {
+    let res: any = await request({
+      url: `/${Por_Anh}`,
+      method: "delete",
+      data,
+    });
+    return res;
+  },
+};
+export default QuanLyAnhService;
