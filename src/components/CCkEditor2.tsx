f78be1e1c754c9018b0b210a4b48d19735c7274e
baovu <vuhuubao22@gmail.com>
2023-01-18 10:26:04 +0700

    add

--- src/components/CCkEditor2.tsx
+++ src/components/CCkEditor2.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
-import { CKEditor } from 'ckeditor4-react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 interface Props {
     key?: any,
@@ -25,7 +26,44 @@ const CCkEditor2 = (props: Props) => {
   } 
     return (
       <>
-        <CKEditor initData="<p>This is an example CKEditor 4 WYSIWYG editor instance.</p>" />
+        <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    config={{
+                        ckfinder: {
+                            // Upload the images to the server using the CKFinder QuickUpload command.
+                            uploadUrl: '/ckeditor5-ckfinder3-react-php/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files&responseType=json',
+                            // Define the CKFinder configuration (if necessary).
+                            options: {
+                                resourceType: 'Images'
+                            }
+                        },
+               
+                      toolbar: [ 'Essentials', 'CKFinderUploadAdapter', 'Autoformat', 'Bold', 'Italic', 'BlockQuote', 'CKBox', 'CKFinder', 'CloudServices', 'EasyImage', 'Heading', 'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload', 'Indent', 'Link', 'List', 'MediaEmbed', 'Paragraph', 'PasteFromOffice', 'PictureEditing', 'Table', 'TableToolbar', 'TextTransformation' ],
+                      heading: {
+                          options: [
+                              { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                              { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                              { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+                          ]
+                      }
+                    }}
+                    onReady={(editor:any) => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={(event:any, editor:any) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={(event:any, editor:any) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={(event:any, editor:any) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+
       </>
     )
 }
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,5 @@
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.gif";
+declare module "@ckeditor/ckeditor5-react";
+declare module "@ckeditor/ckeditor5-build-classic";
