4c652871447515851cfa1a627c035d4e0f77b1cb
Duong Duc Anh <duongdoican@gmail.com>
2023-01-27 16:52:03 +0700

    update

--- src/containers/PortalSite/Pages/KhoaHoc/Reducer.ts
+++ src/containers/PortalSite/Pages/KhoaHoc/Reducer.ts
@@ -2,6 +2,11 @@ import { InitState, IState } from "./InitState";
 export const Reducer = (state: IState = InitState, action: any) => {
   switch (action.type) {
     case "GetItem":
+      let data = action.items;
+      action.items.DanhSachMonHocCon.forEach((val: any) => {
+        val.check = true;
+      });
+      console.log(data);
       return {
         ...state,
         DataItem: action.items,
