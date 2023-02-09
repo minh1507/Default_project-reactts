1a509f05e399b095c8ec0c40f9c5134d4344f108
Duong Duc Anh <duongdoican@gmail.com>
2023-02-09 08:53:57 +0700

    update

--- src/containers/PortalSite/Pages/KhoaHoc/index.tsx
+++ src/containers/PortalSite/Pages/KhoaHoc/index.tsx
@@ -84,6 +84,8 @@ const KhoaHoc = (props: Props) => {
     setAccName(name);
   };
 
+  console.log(pb)
+  
   const handleLoaiKhoaHoc = (Id: string) => {
     Actions.GetKhoaHocTheoLoaiKhoaHoc(Id, dispatch);
     setAccName("");
